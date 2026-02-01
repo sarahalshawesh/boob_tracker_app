16afbe7f78882318c0d0b07d069f7ef6a7ce317d
Sarah Robinson <sarahrose@Sarahs-MacBook-Pro-8.local>
2026-02-01 16:38:05 +0000

    sets up app and main

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # boob_tracker_app
+
+A python app to track which boob was last used for breastfeeding. 
---
+++ main.py
@@ -0,0 +1,12 @@
+
+
+def main():
+    while True:
+        pass
+        
+
+
+
+if __name__ == "__main__":
+    main()
+
