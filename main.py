3848381cca262c4e31355789bbde1bacc710e4f5
Sarah Robinson <sarahrose@Sarahs-MacBook-Pro-8.local>
2026-02-04 17:35:13 +0000

    adds box layout and buttons for boobs

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
+from kivy.app import App 
+from kivy.uix.widget import Widget
+from kivy.uix.button import Button
+from kivy.uix.boxlayout import BoxLayout 
 
+class BoobTracker(Widget):
+    layout = BoxLayout(orientation='horizontal')
+    left_boob = Button(text="left boob")
+    right_boob = Button(text="right boob")
+    layout.add_widget(left_boob)
+    layout.add_widget(right_boob)
 
-def main():
-    while True:
-        pass
-        
-
-
+class BoobApp(App):
+    def build(Self):
+        return BoobTracker()
 
 if __name__ == "__main__":
-    main()
-
+    BoobApp().run()
