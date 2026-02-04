90a2caf8c6fedf57397b512a2636dfa3627ffaac
Sarah Robinson <sarahrose@Sarahs-MacBook-Pro-8.local>
2026-02-04 22:19:47 +0000

    adds kivy file with orientation and buttons

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from kivy.uix.widget import Widget
 from kivy.uix.button import Button
 from kivy.uix.boxlayout import BoxLayout 
 
-class BoobTracker(Widget):
+class BoobTracker(BoxLayout):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         layout = BoxLayout(orientation='horizontal')
